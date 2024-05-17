06a41f62376b3cfcd1e8aeba52262174908abf3c
giuliamari04 <giuliamari04@gmail.com>
2024-05-17 19:20:02 +0200

    filter

--- app/Livewire/HomeClient.php
+++ app/Livewire/HomeClient.php
@@ -7,27 +7,51 @@ use App\Models\Client;
 use App\Models\User;
 use App\Models\Activity;
 use Livewire\WithPagination;
+
 class HomeClient extends Component
 {
     use WithPagination;
 
-public $user;
-public $clients;
-public $activities;
-public $industries;
-public $nameFilter = '';
-public $industryFilter='';
+    public $user;
+    public $clients;
+    public $clientsFiltered;
+    public $activities;
+    public $industries;
+    public $industryFilter = [];
+    public $nameFilter = '';
+
     public function render()
     {
         $this->user = auth()->user();
-        $this->clients = Client::where('user_id', auth()->user()->id)->get();
+        $this->clients = Client::where('user_id', $this->user->id)->get();
         $this->activities = Activity::whereIn('client_id', $this->clients->pluck('id'))->get();
-        $this->industries = Client::pluck('industry')->unique()->filter();
+        $this->industries = Client::where('user_id', $this->user->id)->pluck('industry')->unique()->filter();
 
-        return view('livewire.home-client',[
-           'clients'=> $this->clients,
-            'activities'=>$this->activities,
-            'industries'=>$this->industries,
+        // If no industry filter is applied, show all clients for the authenticated user
+        if (empty($this->industryFilter)) {
+            $this->clientsFiltered = Client::where('user_id', $this->user->id)
+                ->when($this->nameFilter, function ($query) {
+                    $query->where('name', 'like', '%' . $this->nameFilter . '%');
+                })
+                ->get();
+        } else {
+            $this->clientsFiltered = Client::where('user_id', $this->user->id)
+                ->when($this->industryFilter, function ($query) {
+                    $query->whereIn('industry', $this->industryFilter);
+                })
+                ->when($this->nameFilter, function ($query) {
+                    $query->where('name', 'like', '%' . $this->nameFilter . '%');
+                })
+                ->get();
+        }
+
+        return view('livewire.home-client', [
+            'clientsFiltered' => $this->clientsFiltered,
+            'activities' => $this->activities,
+            'industries' => $this->industries,
         ]);
+    }
+    public function RefreshClients(){
+        $this->render();
     }
 }
